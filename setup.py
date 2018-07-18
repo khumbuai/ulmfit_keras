dbebb14b1c61fc353a2a7a7ff0be2b5e4558648a
maxjeblick <jeblick@math.lmu.de>
2018-07-18 18:23:14 +0200

    setup.py added

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from setuptools import find_packages
+
+long_description = '''
+nnn'''
+
+setup(name='keras_lm',
+      version='1.0.0',
+      description='Keras wikipedia-based Language Model',
+      long_description=long_description,
+      author='nnn',
+      author_email='nnn',
+      url='https://github.com/khumbuai/keras_wiki_lm',
+      download_url='https://github.com/khumbuai/keras_wiki_lm',
+      license='MIT',
+      install_requires=[],
+      extras_require={
+      },
+      classifiers=[
+      ],
+      packages=find_packages())
