b872065917c06c01e6049d25d2c93a8290555378
christof <christof.henkel@campus.lmu.de>
2018-07-19 06:49:43 +0200

    renamed train, addded two dropouts, edited readme

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-# keras_wiki_lm
+# keras ULMfit
 Keras wikipedia-based Language Model
 
 
+In progress
+Implementation of Universal Language Model Fine-tuning for Text Classification with keras 
+https://arxiv.org/pdf/1801.06146.pdf
+
 Portation of https://github.com/salesforce/awd-lstm-lm to keras with tensorflow backend
+
+
+https://github.com/fastai/fastai/tree/master/courses/dl2/imdb_scripts
+
+
+trained on wikitext 103 
+
+https://einstein.ai/research/the-wikitext-long-term-dependency-language-modeling-dataset
--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import data
-from utils import batchify, get_batch, data_gen
+from utils import batchify, get_batch, data_gen2
 import numpy as np
 
 import os
@@ -11,63 +11,80 @@ import pickle
 #config.gpu_options.allow_growth = True
 #session = tf.Session(config=config)
 os.environ["CUDA_VISIBLE_DEVICES"]="1"
-from keras.layers import Input, CuDNNLSTM, Embedding, Dense, CuDNNGRU, LSTM
-from keras.models import Model
+from keras.layers import Input, CuDNNLSTM, Embedding, Dense, TimeDistributed, Dropout
+from keras.models import Model, load_model
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras.losses import categorical_crossentropy, sparse_categorical_crossentropy
 from tied_embeddings import TiedEmbeddingsTransposed
 from qrnn import QRNN
-corpus = data.Corpus('assets/wikitext-103/')
-pickle.dump(corpus,open('wikitext-103.corpus','wb'))
+#corpus = data.Corpus('assets/wikitext-103/')
+#pickle.dump(corpus,open('wikitext-103.corpus','wb'))
 
 corpus = pickle.load(open('wikitext-103.corpus','rb'))
 
 
 eval_batch_size = 10
 test_batch_size = 1
-batch_size = 256
+batch_size = 32
 SEQ_LEN = 50
 train_data = batchify(corpus.train, batch_size)
 val_data = batchify(corpus.valid, eval_batch_size)
 #test_data = batchify(corpus.test, test_batch_size)
 
-# save to temp
 
-#data, targets = get_batch(val_data, 0, seq_len=SEQ_LEN)
-#X = np.zeros(shape=(len(corpus.train)-SEQ_LEN -2,SEQ_LEN))
-#y = np.zeros(shape=(len(corpus.train)-SEQ_LEN -2,))
-#for i in tqdm(range(len(corpus.train)-SEQ_LEN -2)):
-#    data, targets = get_batch(corpus.train,i,SEQ_LEN)
-#    X[i] = data
-#    y[i] = targets
+train_gen = data_gen2(train_data,SEQ_LEN, batch_size=batch_size)
+valid_gen = data_gen2(val_data,SEQ_LEN, batch_size=eval_batch_size)
 
-
-train_gen = data_gen(train_data,SEQ_LEN, batch_size=batch_size)
-valid_gen = data_gen(val_data,SEQ_LEN, batch_size=eval_batch_size)
 
 num_words = len(corpus.word2idx) +1
 
-def build_model():
+def build_language_model(dropout=0.4, dropouth=0.3, dropouti=0.5, dropoute=0.1, wdrop=0.5, tie_weights=True):
 
     inp = Input(shape=(None,))
     emb = Embedding(num_words,300)
     emb_inp = emb(inp)
+    emb_inp = Dropout(dropouti)(emb_inp)
     rnn = CuDNNLSTM(1024, return_sequences=True)(emb_inp)
     rnn = CuDNNLSTM(1024, return_sequences=True)(rnn)
-    rnn = CuDNNLSTM(300)(rnn)
-    #rnn = QRNN(256, return_sequences=True)(emb_inp)
-    #rnn = QRNN(256)(rnn)
-    #den = Dense(SEQ_LEN, activation='relu')(rnn)
-    #out = Dense(num_words, activation='softmax')(den)
-    out = TiedEmbeddingsTransposed(tied_to=emb,activation='softmax')(rnn)
+    rnn = CuDNNLSTM(300, return_sequences=True)(rnn)
+
+    if tie_weights:
+        logits = TimeDistributed(TiedEmbeddingsTransposed(tied_to=emb,activation='softmax'))(rnn)
+    else:
+        logits = TimeDistributed(Dense(num_words, activation='softmax'))(rnn)
+    out = Dropout(dropout)(logits)
     model = Model(inputs=inp, outputs=out)
-    #model = multi_gpu_model(model, gpus=2)
-    model.compile(optimizer='adam', loss=sparse_categorical_crossentropy)
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
     model.summary()
     return model
 
-model = build_model()
+early_stop = EarlyStopping(patience=2)
+check_point = ModelCheckpoint('model.hdf5', save_weights_only=True)
+model = build_language_model()
+model.fit_generator(train_gen,
+                    steps_per_epoch=train_data.shape[0]//SEQ_LEN,
+                    epochs=10,
+                    validation_data=valid_gen,
+                    validation_steps=val_data.shape[0]//SEQ_LEN,
+                    callbacks=[early_stop,check_point])
 
-model.fit_generator(train_gen, steps_per_epoch=train_data.shape[0]-SEQ_LEN-1, epochs=1)
 
 
-#model.fit(X,y, batch_size=512)
+#model.evaluate_generator(valid_gen, steps=val_data.shape[0]//SEQ_LEN, verbose=True)
+
+
+test_sentence ='i am sick . hence i go to the'.split()
+encoded_sentence = [corpus.word2idx[w] for w in test_sentence]
+for i in range(5):
+    pred = model.predict(encoded_sentence)
+    answer = np.argmax(pred,axis = 2)
+    encoded_sentence.append([a[0] for a in answer][-1])
+
+print(' '.join([corpus.idx2word[i[0]] for i in answer]))
+print(' '.join([corpus.idx2word[i] for i in encoded_sentence]))
+
+
+
+
+
+#output = TimeDistributed(Dense(opt.word_vocab_size, activation='softmax'))(x)
