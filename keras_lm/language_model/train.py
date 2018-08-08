23e7cd8b55dbf37c54cd2a018ce63aebd7651548
maxjeblick <jeblick@math.lmu.de>
2018-08-08 15:07:09 +0200

    merge conflict in language_model/train.py removed

--- keras_lm/language_model/train.py
+++ keras_lm/language_model/train.py
@@ -7,6 +7,7 @@ import numpy as np
 
 from keras_lm.language_model.model import build_language_model
 from keras_lm.preprocessing.batch_generators import BatchGenerator
+from keras_lm.utils.utils import LoadParameters
 
 
 def evaluate_model(model, word2idx, test_sentence, num_predictions=5):
@@ -35,26 +36,36 @@ def evaluate_model(model, word2idx, test_sentence, num_predictions=5):
 
 
 if __name__ == '__main__':
-    epochs = 20
-    batch_size = 64
-    valid_batch_size = 16
-    seq_len = 50
 
-    corpus = pickle.load(open('assets/wikitext-103/wikitext-103.corpus','rb'))
+    # 1. Load parameters from config.yaml file
+    params = LoadParameters()
+    WIKIPEDIA_CORPUS_FILE = params.params['wikipedia_corpus_file']
+    LANGUAGE_MODEL_FILE = params.params['language_model_file']
+    LANGUAGE_MODEL_PARAMS = params.params['lm_params']
+
+    epochs = params.params['lm_epochs']
+    batch_size = params.params['lm_batch_size']
+    valid_batch_size = params.params['lm_valid_batch_size']
+    seq_len = params.params['lm_seq_len']
+
+    # 2. Load Corpus
+    corpus = pickle.load(open(WIKIPEDIA_CORPUS_FILE, 'rb'))
 
     train_gen = BatchGenerator(corpus.train, batch_size, 'normal', seq_len, modify_seq_len=True)
     valid_gen = BatchGenerator(corpus.valid, valid_batch_size, 'normal', seq_len, modify_seq_len=True)
 
     K.clear_session()
-    num_words = len(corpus.word2idx) +1
+    num_words = len(corpus.word2idx) + 1
 
-    model = build_language_model(num_words, embedding_size=300, use_gpu=False)
-    model.compile(loss='sparse_categorical_crossentropy', optimizer=Adam(lr=3e-4, beta_1=0.8, beta_2=0.99))
+    model = build_language_model(num_words, **LANGUAGE_MODEL_PARAMS)
+    model.compile(loss='sparse_categorical_crossentropy', metrics=['sparse_categorical_accuracy'],
+                  optimizer=Adam(lr=3e-4, beta_1=0.8, beta_2=0.99))
 
     model.summary()
 
     callbacks = [EarlyStopping(patience=5),
-                 ModelCheckpoint('assets/language_model.hdf5', save_weights_only=True)]
+                 ModelCheckpoint(LANGUAGE_MODEL_FILE, save_weights_only=True)]
+
     history = model.fit_generator(train_gen,
                                   steps_per_epoch=len(corpus.train) // (seq_len * batch_size),
                                   epochs=epochs,
